d8af89b316451a400726e5eb85321c3067f9be98
jjmr007 <money.as.flow@gmail.com>
2023-01-29 19:11:15 -0400

    celaning repo

--- deps/_getRawTxHash.js
+++
@@ -1,6 +0,0 @@
-// use: $ node getRawTxHash <rlp-serialized-raw-tx>
-const { ethers } = require("ethers");
-const hAsh = ethers.utils.keccak256;
-const h = (rlp) => hAsh(rlp);
-
-module.exports = {h};
--- scripts/getRawTxHash.js
+++
@@ -1,7 +0,0 @@
-// use: $ node getRawTxHash <rlp-serialized-raw-tx>
-const { ethers } = require("ethers");
-const PARAMETERS = process.argv;
-const hAsh = ethers.utils.keccak256;
-const h = hAsh(PARAMETERS[2]);
-
-console.log('ethereum hash value: ' + h);
